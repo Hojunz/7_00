91369592aaeb49fa8021d4bc7d47e30891b7d972
kingseungil <flykim5115@gmail.com>
2022-12-06 20:12:54 +0900

    메인페이지 (로그인,로그아웃 기능구현)

--- app.py
+++ app.py
@@ -10,64 +10,84 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 SECRET_KEY = 'Zoo'
 
+# 메인 페이지
+
+
 @app.route('/')
 def home():
-	return render_template('index.html')
+    return render_template('index.html')
+
+# 로그인 페이지
 
 
 @app.route('/login')
 def login():
-	return render_template('login.html')
+    return render_template('login.html')
+
+# 로그인
+
 
 @app.route('/log_in', methods=['POST'])
 def log_in():
-	email_receive = request.form["email_give"]
-	password_receive = request.form["password_give"]
-	# print(email_receive)
-	# print(password_receive)
+    email_receive = request.form["email_give"]
+    password_receive = request.form["password_give"]
 
-	db = pymysql.connect(host='localhost', user='root', db='flask_test', password='12345678', charset='utf8')
-	curs = db.cursor()
+    db = pymysql.connect(host='localhost', user='root',
+                         db='test', password='0000', charset='utf8')
+    curs = db.cursor()
 
-	sql = """
+    sql = """
 		SELECT *
 		FROM users u
 		where u.email = %s
 		"""
 
-	curs.execute(sql, email_receive)
+    curs.execute(sql, email_receive)
 
-	users_result = curs.fetchall()
-	# print(users_result[0][1] != password_receive)
+    users_result = curs.fetchall()
 
-	json_str = json.dumps(users_result, indent=4, sort_keys=True, default=str)
-	db.commit()
-	db.close()
+    # json_str = json.dumps(users_result, indent=4, sort_keys=True, default=str)
+    db.commit()
+    db.close()
 
-	if users_result[0][1] != password_receive:
-		msg = "비밀번호가 일치하지 않습니다. 다시 로그인해주세요."
-		return jsonify({'result': 'fail', 'msg': msg})
-	else:
-		# 토큰 발행, id, payload, 시크릿키가 필요
-		payload = {
-			'id': email_receive,
-			'exp': datetime.utcnow() + timedelta(seconds=30)
-		}
-		token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
-		# print(token)
-		return jsonify({'result': 'success', 'token': token})
+    if users_result[0][2] != password_receive:
+        msg = "비밀번호가 일치하지 않습니다. 다시 로그인해주세요."
+        return jsonify({'result': 'fail', 'msg': msg})
+
+    else:
+        # 토큰 발행, id, payload, 시크릿키가 필요
+        payload = {
+            'id': email_receive,
+            'exp': datetime.utcnow() + timedelta(seconds=60*60*24)
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm='HS256')
+        return jsonify({'result': 'success', 'token': token})
+
+
+# 게시판 작성 페이지
+
+
+@app.route('/write_page')
+def write_page():
+    return render_template('write_page.html')
+
+# 게시글 작성
+
 
 @app.route('/user_only', methods=['POST'])
 def user_only():
-	token_receive = request.cookies.get('mytoken')
-	print(token_receive)
+    token_receive = request.cookies.get('mytoken')
+    print(token_receive)
 
-	try:
-		payload = jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
-		msg = payload['id'] + '님은 게시물을 작성하실 수 있습니다.'
-		return jsonify({'msg': msg})
-	except(jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-		return jsonify({'msg': 'User Only Access!!!'})
+    try:
+        payload = jwt.decode(token_receive, SECRET_KEY,
+                             algorithms=['HS256'])
+        msg = payload['id'] + '님은 게시물을 작성하실 수 있습니다.'
+        return jsonify({'msg': msg})
+        # return redirect("/templates/write_page.html")
+    except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
+        return jsonify({'msg': 'User Only Access!!!'})
+
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('127.0.0.1', port=5000, debug=True)
--- static/common_js.js
+++ static/common_js.js
@@ -1,3 +1,5 @@
 function sign_out() {
-    $.removeCookie('mytoken', {path: '/'}); // path : 경로설정이다. 이 사이트의 모든 페이지가 해당된다면 / 이렇게 슬러시만 둔다. 그렇지 않고 특정 폴더라면 경로를 넣으면 된다.
+  $.removeCookie("mytoken", "", { expires: -1, path: "/" }); // path : 경로설정이다. 이 사이트의 모든 페이지가 해당된다면 / 이렇게 슬러시만 둔다. 그렇지 않고 특정 폴더라면 경로를 넣으면 된다.
+  alert("로그아웃!");
+  window.location.href = "/login";
 }
