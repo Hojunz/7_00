80a37a49eb55b63b804b83349c2f29a92b92e57a
kingseungil <flykim5115@gmail.com>
2022-12-07 17:31:21 +0900

    게시판 불러오기, 쓰기

--- app.py
+++ app.py
@@ -10,24 +10,24 @@ app.config["TEMPLATES_AUTO_RELOAD"] = True
 
 SECRET_KEY = 'Zoo'
 
-# 메인 페이지
+#! 메인 페이지
 
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-# 로그인 페이지
+#! 로그인 페이지
 
 
 @app.route('/login')
 def login():
     return render_template('login.html')
 
-# 로그인
+#! 로그인
 
 
-@app.route('/log_in', methods=['POST'])
+@app.route('/users/login', methods=['POST'])
 def log_in():
     email_receive = request.form["email_give"]
     password_receive = request.form["password_give"]
@@ -64,41 +64,45 @@ def log_in():
         return jsonify({'result': 'success', 'token': token})
 
 
-# 게시판 작성 페이지
+#! 게시판 작성 페이지
 
 
 @app.route('/write_page')
 def write_page():
     return render_template('write_page.html')
 
-# 게시글 작성(로그인한 사람만 접근 가능)
+#! 사용자만 게시판 작성 페이지로 이동
 
 
 @app.route('/user_only', methods=['POST'])
-def user_only():
+def move_to_write_page():
     token_receive = request.cookies.get('mytoken')
-    print(token_receive)
 
     try:
-        payload = jwt.decode(token_receive, SECRET_KEY,
-                             algorithms=['HS256'])
-        msg = payload['id'] + '님은 게시물을 작성하실 수 있습니다.'
-        return jsonify({'msg': msg})
-        # return redirect("/templates/write_page.html")
+        jwt.decode(token_receive, SECRET_KEY, algorithms=['HS256'])
+        return jsonify({'result': 'success'})
     except (jwt.ExpiredSignatureError, jwt.exceptions.DecodeError):
-        return jsonify({'msg': 'User Only Access!!!'})
+        return jsonify({'result': 'fail', 'msg': '로그인을 먼저 진행해주세요!!'})
 
-# 게시글 불러오기
+#! 게시글 수정 페이지
 
 
-@app.route('/getpost')
+@app.route('/write_page_update')
+def write_page_update():
+    return render_template('write_page_update.html')
+
+
+#! 게시글 불러오기
+
+
+@app.route('/posts/list')
 def get_post():
     db = pymysql.connect(host='localhost', user='root',
                          db='test', password='0000', charset='utf8')
     curs = db.cursor()
 
     sql = """
-		SELECT title,content,topic
+		SELECT title,content,topic,file
 		FROM posts p
 		"""
 
@@ -111,11 +115,45 @@ def get_post():
 
     return jsonify({'post_list': post_list})
 
-# 게시글 작성
+#! 게시글 작성
 
 
-@app.route("/savepost", methods=["POST"])
+@app.route("/posts/save", methods=["POST"])
 def save_post():
+    title = request.form['title']
+    topic = request.form['topic']
+    content = request.form['content']
+    file = request.files['file']
+
+    print(request.form)
+    extension = file.filename.split('.')[-1]
+    today = datetime.now()
+    mytime = today.strftime("%Y-%m-%d-%H-%M-%S")
+    filename = f'file-{mytime}'
+    save_to = f'{filename}.{extension}'
+    file.save(f'static/images/{save_to}')
+
+    db = pymysql.connect(host='localhost', user='root',
+                         db='test', password='0000', charset='utf8')
+    curs = db.cursor()
+
+    sql = """
+		INSERT INTO posts (title,topic,content,file) VALUES (%s,%s,%s,%s)
+		"""
+
+    curs.execute(sql, (title, topic, content, save_to))
+
+    db.commit()
+    db.close()
+
+    return jsonify({'msg': '게시글 작성 완료!'})
+
+# 게시글 업데이트
+
+
+@app.route('/updatepost', methods=["PUT"])
+def update():
+    post_id = request.form['post_id']
     title = request.form['title']
     topic = request.form['topic']
     content = request.form['content']
@@ -125,15 +163,15 @@ def save_post():
     curs = db.cursor()
 
     sql = """
-		INSERT INTO posts (title,topic, content) VALUES (%s,%s,%s)
+		UPDATE posts SET title = %s, topic = %s, content = %s WHERE post_id = %s
 		"""
 
-    curs.execute(sql, (title, topic, content))
+    curs.execute(sql, (title, topic, content, post_id))
 
     db.commit()
     db.close()
 
-    return jsonify({'msg': '게시글 작성 완료'})
+    return jsonify({'msg': '게시글 수정 완료'})
 
 
 if __name__ == '__main__':
